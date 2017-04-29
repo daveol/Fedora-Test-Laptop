4f733e91ab691b66a74a5e9cbaa4a81c71b5557b
Marthe Veldhuis <msveldhuis96@gmail.com>
2017-04-29 17:52:02 +0200

    Added connection and scan tests, changed wifi data

---
+++ tests/connectiontest.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+import subprocess as subp
+import yaml
+from avocado import Test
+
+class WifiTest_ConnectAP(Test):
+    def test(self):
+        with open("data/wifi_data.yaml", 'r') as stream:
+            try:
+                wifidata = yaml.load(stream)
+            except yaml.YAMLError as exc:
+                self.log.debug(exc)
+        accessPoint = wifidata['access_point_1']['ap1_ssid']
+        accessPointPass = wifidata['access_point_1']['ap1_pass']
+        self.interface = wifidata['wireless_interface']
+
+        self.checkCon(accessPoint, accessPointPass)
+
+    def tryCon(self, expected):
+        # get the SSID connected on the interface
+        p = subp.Popen(['iwgetid', self.interface, '-r'], stdout=subp.PIPE, stderr=subp.PIPE)
+
+        stdout, stderr = p.communicate()
+        activeCon = stdout.rstrip()
+
+        if activeCon != expected:
+            self.fail("initial network connection state was not as expected was: {0} expected {1}".format(activeCon, expected))
+        self.log.debug("Current network checked. Now on {0}".format(activeCon))
+        return activeCon
+
+    def checkCon(self, ssid, password):
+        check = subp.call(['nmcli', 'dev', 'wifi', 'con', ssid, 'password', password])
+        
+        active = self.tryCon(ssid)
+        p = subp.call(['ping', '-I', self.interface, '8.8.8.8', '-c', '1'])
+        
+        if p == 0:
+            self.log.debug("internet is working on network {0}".format(active))
+        else:
+            self.fail("Internet is not available on network {0}".format(active))
+
+
---
+++ tests/scantest.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+import subprocess as subp
+import yaml
+from avocado import Test
+
+class WifiTest_ScanAP(Test):
+    def test(self):
+        with open("data/wifi_data.yaml", 'r') as stream:
+            try:
+                wifidata = yaml.load(stream)
+            except yaml.YAMLError as exc:
+                self.log.debug(exc)
+        ap1 = wifidata['access_point_1']['ap1_ssid']
+        ap2 = wifidata['access_point_2']['ap2_ssid']
+        self.interface = wifidata['wireless_interface']
+
+        self.scanAP(ap1, ap2);
+
+    def scanAP(self, ap1, ap2):
+        p = subp.Popen(['nmcli', 'device', 'wifi', 'device'], stdout=subp.PIPE, stderr=subp.PIPE)
+        
+        stdout, stderr = p.communicate()
+        scan1 = stdout.rstrip()
+
+        if ap1 not in scan1:
+            self.fail("First AP not found {0}".format(ap1))
+        if ap2 not in scan1:
+            self.fail("Second AP not found {0}".format(ap2))
+        else:
+            self.log.debug("Both APs can be found")
+
