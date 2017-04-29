9bced9e51877d2a026901bbab434885da58a19d2
Nick Dekker <nick@freshcoders.nl>
2017-04-29 14:46:15 +0200

    added yaml config for wifi and improved test

--- tests/wifi_ap_switch.py
+++ tests/wifi_ap_switch.py
@@ -1,24 +1,46 @@
 #!/usr/bin/env python
 import subprocess as subp
+import yaml
 from avocado import Test
 
 class WifiTest(Test):
-
     def test(self):
-        switch1 = subp.call(['nmcli', 'dev', 'wifi', 'con', 'nick', 'password', 'yoloyolo'])
+        with open("data/wifi_data.yaml", 'r') as stream:
+            try:
+                wifidata = yaml.load(stream)
+            except yaml.YAMLError as exc:
+                self.log.debug(exc)
+        switchFrom = wifidata['switch_from']['ssid']
+        switchTo = wifidata['switch_to']['ssid']
+        switchFromPass = wifidata['switch_from']['pass']
+        switchToPass = wifidata['switch_to']['pass']
+        self.interface = wifidata['wireless_interface']
 
-        p = subp.call(['ping', '-I', 'wlan0', '8.8.8.8', '-c', '1'])
+        self.switchCon(switchFrom, switchFromPass)
+        self.switchCon(switchTo, switchToPass)
+
+
+    def tryCon(self, expected):
+        # get the SSID connected on the interface
+        p = subp.Popen(['iwgetid', self.interface, '-r'], stdout=subp.PIPE, stderr=subp.PIPE)
+
+        stdout, stderr = p.communicate()
+        activeCon = stdout.rstrip()
+
+        if activeCon != expected:
+            self.fail("initial network connection state was not as expected was: {0} expected {1}".format(activeCon, expected))
+        self.log.debug("Current network checked. Now on {0}".format(activeCon))
+        return activeCon
+
+    def switchCon(self, ssid, password):
+        switch1 = subp.call(['nmcli', 'dev', 'wifi', 'con', ssid, 'password', password])
+
+        active = self.tryCon(ssid)
+        p = subp.call(['ping', '-I', self.interface, '8.8.8.8', '-c', '1'])
 
         if p == 0:
-            self.log.debug("internet is working")
+            self.log.debug("internet is working on network {0}".format(active))
         else:
-            self.fail("Internet is not available")
+            self.fail("Internet is not available on network {0}".format(active))
 
-        switch2 = subp.call(['nmcli', 'dev', 'wifi', 'con', 'nasakeni', 'password', 'mzsv1425'])
 
-        p2 = subp.call(['ping', '-I', 'wlan0', '8.8.8.8', '-c', '1'])
-
-        if p2 == 0:
-            self.log.debug("internet is working %s")
-        else:
-            self.fail("Internet is not available")
