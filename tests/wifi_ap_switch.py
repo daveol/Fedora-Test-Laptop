302df2ce4d36f65548fcb181ff76ff1e5eca4445
Nick Dekker <nick@freshcoders.nl>
2017-04-30 02:35:57 +0200

    updated wifiapswitch test

--- tests/wifi_ap_switch.py
+++ tests/wifi_ap_switch.py
@@ -3,17 +3,17 @@ import subprocess as subp
 import yaml
 from avocado import Test
 
-class WifiTest(Test):
+class WifiAPSwitch(Test):
     def test(self):
         with open("data/wifi_data.yaml", 'r') as stream:
             try:
                 wifidata = yaml.load(stream)
             except yaml.YAMLError as exc:
                 self.log.debug(exc)
-        switchFrom = wifidata['switch_from']['ssid']
-        switchTo = wifidata['switch_to']['ssid']
-        switchFromPass = wifidata['switch_from']['pass']
-        switchToPass = wifidata['switch_to']['pass']
+        switchFrom = wifidata['access_point_1']['ssid']
+        switchTo = wifidata['access_point_2']['ssid']
+        switchFromPass = wifidata['access_point_1']['pass']
+        switchToPass = wifidata['access_point_2']['pass']
         self.interface = wifidata['wireless_interface']
 
         self.switchCon(switchFrom, switchFromPass)
@@ -25,6 +25,8 @@ class WifiTest(Test):
         p = subp.Popen(['iwgetid', self.interface, '-r'], stdout=subp.PIPE, stderr=subp.PIPE)
 
         stdout, stderr = p.communicate()
+
+        # trim extraneous whitespace
         activeCon = stdout.rstrip()
 
         if activeCon != expected:
@@ -33,14 +35,55 @@ class WifiTest(Test):
         return activeCon
 
     def switchCon(self, ssid, password):
-        switch1 = subp.call(['nmcli', 'dev', 'wifi', 'con', ssid, 'password', password])
+        # get all known networks
+        p = subp.Popen(['nmcli', '-t', '--fields', 'NAME,UUID,ACTIVE,TYPE', 'c'], stdout=subp.PIPE, stderr=subp.PIPE)
 
+        stdout, stderr = p.communicate()
+
+        # something went wrong while getting the networks
+        if stderr != "":
+           self.fail("Getting known network list failed {0}".format(stderr))
+
+        # each connection is seperated by '\n'
+        connectionList = stdout.split("\n")
+        existing = False
+
+        """ 
+        we check for the existance of the ssid in the known networks
+        if the network ssid is found, it will connect using its UUID
+        if not found, a new connection will be created and connected to
+        """
+        for con in connectionList:
+            cParts = con.split(":") # nmcli -t output is seperated by :
+            # see if the ssid exist, and has the correct type
+            if ssid in cParts and cParts[3] == "802-11-wireless":
+               if cParts[2] != "yes": # yes means active => don't reconnect
+                  subp.call(['nmcli', 'con', 'up', 'uuid', cParts[1]]) # cParts[1] contains uuid
+               existing = True
+
+        # when the network does not yet exist, create a new one
+        if existing == False:
+           switch = subp.call(['nmcli', 'dev', 'wifi', 'con', ssid, 'password', password])
+
+        # see if the actual connection is still the same as expected
         active = self.tryCon(ssid)
-        p = subp.call(['ping', '-I', self.interface, '8.8.8.8', '-c', '1'])
+
+        # get the default gateway by parsing ip route's output
+        gatewayP1 = subp.Popen(['ip', 'route'], stdout=subp.PIPE, stderr=subp.PIPE)
+        gatewayP2 = subp.Popen(['awk', r'/default/ { print $3 }'], stdin=gatewayP1.stdout, stdout=subp.PIPE, stderr=subp.PIPE)
+
+        gateway, stderr = gatewayP2.communicate()
+
+        # something went wrong while getting the gateway
+        if stderr != "":
+           self.fail("Getting gateway failed {0}".format(stderr))
+
+        # ping default gateway using the desired interface once then check for success
+        p = subp.call(['ping', '-I', self.interface, gateway, '-c', '1'])
 
         if p == 0:
-            self.log.debug("internet is working on network {0}".format(active))
+            self.log.debug("Internet is working on network {0} pinged {1}".format(active, gateway))
         else:
-            self.fail("Internet is not available on network {0}".format(active))
+            self.fail("Internet is not available on network {0} tried to ping {1}".format(active, gateway))
 
 
