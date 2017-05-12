67651747c84c736048899a0f2d419719d117e001
Marthe Veldhuis <msveldhuis96@gmail.com>
2017-05-12 21:16:21 +0200

    Addedd get_gateway method to internet utils, adjusted tests accordingly

--- tests/ethernet_connect.py
+++ tests/ethernet_connect.py
@@ -1,21 +1,17 @@
 #!/usr/bin/env python
 import subprocess as subp
 import yaml
-from internet_utils import InternetUtils
-from avocado import Test
+from utils import internet
+from utils import utils
 
 class EthernetConnect(Test):
     def test(self):
-        internetdata = InternetUtils.load_yaml(self, "data/internet_data.yaml")
+        internetdata = utils.load_yaml(self, "data/internet_data.yaml")
 
         self.interface = internetdata['wired_interface']
 
-        self.connect_and_check()
+        gateway = internet.get_gateway(self.interface, self)
 
-    def connect_and_check(self):
+        pingResult = internet.pingtest_hard(gateway, self.interface)
 
-        pingResult = InternetUtils.pingtest('8.8.8.8', self.interface)
-
-        if pingResult != 0:
-            self.fail("Internet is not available on ethernet interface {0}".format(self.interface))
-        self.log.debug("internet is working on ethernet interface {0}".format(self.interface))
+        self.log.debug("Internet is working on ethernet interface {0}".format(self.interface))
