95b3cb17aa49616133480cbc60db87ca581c700d
gliquilung <blazinramirez@gmail.com>
2017-05-15 10:56:10 +0200

    Updated the test according to the comment on trello
    I have added the load YAML. The test will now only succeed if it scans a device that is defined in the YAML file.

--- tests/bluetooth_scan_test.py
+++ tests/bluetooth_scan_test.py
@@ -2,18 +2,21 @@
 import os
 from avocado import Test
 import bluetooth
+from utils import utils
 
 '''
-This test will scan for available bluetooth devices. If devices are found,
-the test will succeed. The results of a successfully test are in the log.
-If this test fails, it will abort  and raise an exception.
+This test will scan for available bluetooth devices and checks if the
+bluetooth device that is specified in the YAML file is available.
 '''
 
 class BluetoothScanTest(Test):
   def test():
+      testdata = utils.load_yaml(self, "data/bluetooth_data.yaml")
+      self.targetDeviceMac = testdata['addr']
+
       results = bluetooth.discover_devices(lookup_names = True)
 
-      if (results ==None):
+      if self.targetDeviceMac not in results:
           self.fail("Bluetooth Scan test failed")
 
-      self.log.debug("Bluetooth ping test succeeded: " + results)
+      self.log.debug("Bluetooth Scan test succeeded: " + results)
