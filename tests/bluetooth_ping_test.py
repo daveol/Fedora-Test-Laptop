91fad967c86e3dbffd321ab595fb021436402d37
gliquilung <blazinramirez@gmail.com>
2017-05-17 14:40:51 +0200

    Update bluetooth_ping_test.py

--- tests/bluetooth_ping_test.py
+++ tests/bluetooth_ping_test.py
@@ -13,7 +13,7 @@ If the test fails, a fail exception will be raised.
 class BluetoothPingTest(Test):
     def test(self):
       testdata = utils.load_yaml(self, "data/bluetooth_data.yaml")
-      self.targetDeviceMac = testdata['addr']
+      self.targetDeviceMac = testdata['testdata']['addr']
 
       p = subp.Popen(['sudo', 'l2ping', self.targetDeviceMac ,'-c', '5'], stdout=subp.PIPE, stderr=subp.PIPE)
       result = p.communicate()[0]
