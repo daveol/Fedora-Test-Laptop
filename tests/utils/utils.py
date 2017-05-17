f7110b4c8963a73f30d58a114942d43fe6be263c
Nick Dekker <nick@freshcoders.nl>
2017-05-17 19:44:52 +0200

    Updated display test

--- tests/utils/utils.py
+++ tests/utils/utils.py
@@ -1,4 +1,13 @@
 import yaml
+from avocado.core import settings
+import os.path
+
+def get_data_dir():
+    # Try relative to this in case it is not properly installed
+    _path = os.path.join(os.path.dirname(__file__), '..', 'data')
+    if not os.path.isdir(_path):
+        _path = settings.settings.get_value('datadir.paths', 'data_dir', 'path')
+    return _path
 
 def load_yaml(test_class, path):
     with open(path, 'r') as stream:
