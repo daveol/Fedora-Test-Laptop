e289ced688f17ea6a54d833db529dc410ec190f2
gliquilung <blazinramirez@gmail.com>
2017-05-22 11:02:30 +0200

    Update bluetooth_ping.py

--- tests/bluetooth_ping.py
+++ tests/bluetooth_ping.py
@@ -14,11 +14,15 @@ class BluetoothPing(Test):
       testdata = utils.load_yaml(self, "data/bluetooth_data.yaml")
       self.targetDeviceMac = testdata['testdata']['addr']
 
-      p = subp.Popen(['sudo', 'l2ping', self.targetDeviceMac ,'-c', '5'], stdout=subp.PIPE, stderr=subp.PIPE)
-      result = p.communicate()[0]
-      returnCode = p.returncode
+      p = pingtest()
 
-      if returnCode != 0 :
+      if p != 0 :
           self.fail("Could not ping " + self.targetDeviceMac)
 
      self.log.debug("Ping test working {0}".format(result))
+    
+    def pingtest(self)
+        p= subp.Popen(['sudo', 'l2ping', self.targetDeviceMac ,'-c', '5'], stdout=subp.PIPE, stderr=subp.PIPE)
+        result = p.communicate()[0]
+        returnCode = p.returncode
+        return returnCode
