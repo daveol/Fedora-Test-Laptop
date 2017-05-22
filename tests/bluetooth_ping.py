42d9a7919bbab16d7e6ebda8f7889c56a51d0446
gliquilung <blazinramirez@gmail.com>
2017-05-22 10:54:59 +0200

    Update and rename bluetooth_ping_test.py to bluetooth_ping.py

--- tests/bluetooth_ping.py
+++ tests/bluetooth_ping.py
@@ -2,7 +2,6 @@
 import os
 import subprocess as subp
 from avocado import Test
-import bluetooth
 from utils import utils
 
 '''
@@ -10,7 +9,7 @@ This test will ping a specific device. The device needs to be specified in the
 YAML file. If the device responds, the result will be found in the debug log.
 If the test fails, a fail exception will be raised.
 '''
-class BluetoothPingTest(Test):
+class BluetoothPing(Test):
     def test(self):
       testdata = utils.load_yaml(self, "data/bluetooth_data.yaml")
       self.targetDeviceMac = testdata['testdata']['addr']
