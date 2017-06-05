10f25a0f9ed0dc07047936dc10807cefda159a8c
Marthe Veldhuis <msveldhuis96@gmail.com>
2017-06-05 22:21:12 +0200

    Moved gdk init and gtk main to run after pipeline creationg, render only one buffer.

--- tests/webcam_read_qr.py
+++ tests/webcam_read_qr.py
@@ -31,29 +31,32 @@ class WebcamReadQR(Test):
             # self.skip("No webcam detected: /dev/video0 cannot be found");
         self.device = '/dev/video0'
         
-        Gdk.threads_init()
-        gtk.main()
         self.take_snapshot()
 
     def test(self):
         self.create_video_pipeline()
+        Gdk.threads_init()
+        gtk.main()
        
     def create_video_pipeline(self):
         gst.init([])
         #v4l2src
-        self.video_player = gst.parse_launch("videotestsrc ! jpegenc ! filesink location=cam.jpg")
-        self.video_player.set_state(gst.State.PLAYING)
+        self.video_player = gst.parse_launch("videotestsrc num-buffers=1000 ! jpegenc ! filesink location=cam.jpg")
         
         bus = self.video_player.get_bus()
         bus.add_signal_watch()
         bus.connect("message", self.on_message)
         bus.enable_sync_message_emission()
-        bus.connect("sync-message::element", self.on_sync_message)        
+        bus.connect("sync-message::element", self.on_sync_message)   
+        self.video_player.set_state(gst.State.PLAYING)     
 
     def on_message(self, bus, message):
         t = message.type
+        self.log.debug("EOS")
         if t == gst.MessageType.EOS:
             self.exit()
+            self.log.debug("EOS")
+            #TODO: read qr code
         elif t == gst.MessageType.ERROR:
             self.exit()
             self.fail("Error {0}".format(message.parse_error()))
@@ -68,4 +71,5 @@ class WebcamReadQR(Test):
         gtk.main_quit()
         
     def take_snapshot(self):
+        pass
         #TODO:fill this in
