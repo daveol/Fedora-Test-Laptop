056482eee65126d87022c1a6cd9b9f4245c1068e
Nick Dekker <nick@freshcoders.nl>
2017-06-07 13:35:50 +0200

    Added read qr in cam test

--- tests/webcam_read_qr.py
+++ tests/webcam_read_qr.py
@@ -4,12 +4,9 @@
 This module sets up a video stream from internal or connected webcam using Gstreamer.
 You can then take snapshots.
 
-import qrtools
-qr = qrtools.QR()
-qr.decode("cam.jpg")
-print qr.data
 
 """
+import qrtools
 import gi
 gi.require_version('Gtk', '3.0')
 gi.require_version('Gst', '1.0')
@@ -23,53 +20,51 @@ from os.path import exists, relpath
 import qrtools
 import time
 
-#import pyqrcode
-
-class WebcamReadQR(Test):    
+class WebcamReadQR(Test):
     def setUp(self):
         # if not exists('/dev/video0'):
             # self.skip("No webcam detected: /dev/video0 cannot be found");
         self.device = '/dev/video0'
-        
+
         self.take_snapshot()
 
     def test(self):
         self.create_video_pipeline()
         Gdk.threads_init()
         gtk.main()
-       
+        if (self.qr_data != "test"):
+            self.fail(3)
+
     def create_video_pipeline(self):
         gst.init([])
         #v4l2src
-        self.video_player = gst.parse_launch("videotestsrc num-buffers=1000 ! jpegenc ! filesink location=cam.jpg")
-        
+        self.video_player = gst.parse_launch("v4l2src num-buffers=10 ! jpegenc ! filesink location=cam_f.jpg")
+
         bus = self.video_player.get_bus()
         bus.add_signal_watch()
         bus.connect("message", self.on_message)
         bus.enable_sync_message_emission()
-        bus.connect("sync-message::element", self.on_sync_message)   
-        self.video_player.set_state(gst.State.PLAYING)     
+        bus.connect("sync-message::element", self.on_sync_message)
+        self.video_player.set_state(gst.State.PLAYING)
 
     def on_message(self, bus, message):
         t = message.type
-        self.log.debug("EOS")
         if t == gst.MessageType.EOS:
             self.exit()
-            self.log.debug("EOS")
-            #TODO: read qr code
+
+            time.sleep(3)
+            qr = qrtools.QR()
+            qr.decode("cam.jpg")
+            self.log.debug(qr.data)
+            self.qr_data = qr.data
         elif t == gst.MessageType.ERROR:
             self.exit()
             self.fail("Error {0}".format(message.parse_error()))
 
-    def on_sync_message(self, bus, message):
-        if message.structure is None:
-            return
-        message_name = message.structure.get_name()
-                   
     def exit(self):
-        self.video_player.set_state(gst.State.NULL) 
+        self.video_player.set_state(gst.State.NULL)
         gtk.main_quit()
-        
+
     def take_snapshot(self):
         pass
         #TODO:fill this in
