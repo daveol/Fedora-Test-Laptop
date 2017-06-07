86f5a81842219a74d34b530cd31f8036d3115114
Nick Dekker <nick@freshcoders.nl>
2017-06-07 13:57:47 +0200

    merged some imports together

--- tests/webcam_read_qr.py
+++ tests/webcam_read_qr.py
@@ -10,14 +10,10 @@ import qrtools
 import gi
 gi.require_version('Gtk', '3.0')
 gi.require_version('Gst', '1.0')
-from gi.repository import Gtk as gtk
-from gi.repository import Gdk
-from gi.repository import Gst as gst
-from gi.repository import GdkPixbuf
+from gi.repository import Gtk, Gdk, Gst
 
 from avocado import Test
 from os.path import exists, relpath
-import qrtools
 import time
 
 class WebcamReadQR(Test):
@@ -31,25 +27,23 @@ class WebcamReadQR(Test):
     def test(self):
         self.create_video_pipeline()
         Gdk.threads_init()
-        gtk.main()
+        Gtk.main()
         if (self.qr_data != "test"):
             self.fail(3)
 
     def create_video_pipeline(self):
-        gst.init([])
+        Gst.init([])
         #v4l2src
-        self.video_player = gst.parse_launch("v4l2src num-buffers=10 ! jpegenc ! filesink location=cam_f.jpg")
+        self.video_player = Gst.parse_launch("v4l2src num-buffers=10 ! jpegenc ! filesink location=cam_f.jpg")
 
         bus = self.video_player.get_bus()
         bus.add_signal_watch()
         bus.connect("message", self.on_message)
-        bus.enable_sync_message_emission()
-        bus.connect("sync-message::element", self.on_sync_message)
-        self.video_player.set_state(gst.State.PLAYING)
+        self.video_player.set_state(Gst.State.PLAYING)
 
     def on_message(self, bus, message):
         t = message.type
-        if t == gst.MessageType.EOS:
+        if t == Gst.MessageType.EOS:
             self.exit()
 
             time.sleep(3)
@@ -57,14 +51,14 @@ class WebcamReadQR(Test):
             qr.decode("cam.jpg")
             self.log.debug(qr.data)
             self.qr_data = qr.data
-        elif t == gst.MessageType.ERROR:
+        elif t == Gst.MessageType.ERROR:
             self.exit()
             self.fail("Error {0}".format(message.parse_error()))
 
     def exit(self):
-        self.video_player.set_state(gst.State.NULL)
-        gtk.main_quit()
+        self.video_player.set_state(Gst.State.NULL)
+        Gtk.main_quit()
 
     def take_snapshot(self):
         pass
-        #TODO:fill this in
+        #TODO:fill this in, probably with set state to PLAYING
